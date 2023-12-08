478ac9da46824a8ec9a535d7c176731ff5352472
DELL <khanhlinhk52thd@gmail.com>
2023-12-09 01:10:32 +0700

    stash code

---
+++ app/src/main/java/com/example/appquizlet/Change_language.kt
@@ -0,0 +1,11 @@
+package com.example.appquizlet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Change_language : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_language)
+    }
+}
---
+++ app/src/main/java/com/example/appquizlet/Change_username.kt
@@ -0,0 +1,23 @@
+package com.example.appquizlet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appquizlet.databinding.ActivityChangeUsernameBinding
+
+private lateinit var binding: ActivityChangeUsernameBinding
+class Change_username : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_username)
+
+        binding = ActivityChangeUsernameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSendChangeUsername.setOnClickListener {
+            val btn_send_username = Intent(this,Ch_username::class.java)
+            startActivity(btn_send_username)
+        }
+
+    }
+}
